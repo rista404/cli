018f1ad722c225387d34db35d9aa0b260377f24e
vilmibm <vilmibm@neongrid.space>
2020-02-24 14:23:34 -0600

    start on repo fork

--- command/repo.go
+++ command/repo.go
@@ -2,6 +2,7 @@ package command
 
 import (
 	"fmt"
+	"net/url"
 	"strings"
 
 	"github.com/cli/cli/internal/ghrepo"
@@ -12,6 +13,7 @@ import (
 func init() {
 	RootCmd.AddCommand(repoCmd)
 	repoCmd.AddCommand(repoViewCmd)
+	repoCmd.AddCommand(repoForkCmd)
 }
 
 var repoCmd = &cobra.Command{
@@ -25,12 +27,64 @@ A repository can be supplied as an argument in any of the following formats:
 }
 
 var repoViewCmd = &cobra.Command{
-	Use:   "view [<repo>]",
-	Short: "View a repository in the browser",
+	Use:   "view [<repository>]",
+	Short: "View a repository in the browser.",
 	Long: `View a GitHub repository in the browser.
 
 With no argument, the repository for the current directory is opened.`,
 	RunE: repoView,
+}
+
+var repoForkCmd = &cobra.Command{
+	Use:   "fork [<repository>]",
+	Short: "Create a fork of a repository.",
+	Long: `Create a fork of a repository.
+
+With no argument, creates a fork of the current repository and adds a remote. Otherwise, forks the specified repository.`,
+	RunE: repoFork,
+}
+
+func isURL(arg string) bool {
+	return strings.HasPrefix(arg, "http:/") || strings.HasPrefix(arg, "https:/")
+}
+
+func repoFork(cmd *cobra.Command, args []string) error {
+	ctx := contextForCommand(cmd)
+
+	var toFork ghrepo.Interface
+	if len(args) == 0 {
+		baseRepo, err := determineBaseRepo(cmd, ctx)
+		if err != nil {
+			return fmt.Errorf("unable to determine base repository: %w", err)
+		}
+		// TODO check if user already has a fork
+		toFork = baseRepo
+	} else {
+		repoArg := args[0]
+
+		if isURL(repoArg) {
+			parsedURL, err := url.Parse(repoArg)
+			if err != nil {
+				return fmt.Errorf("did not understand argument: %w", err)
+			}
+
+			toFork, err = ghrepo.FromURL(parsedURL)
+			if err != nil {
+				return fmt.Errorf("did not understand argument: %w", err)
+			}
+
+		} else {
+			toFork = ghrepo.FromFullName(repoArg)
+			if toFork.RepoName() == "" || toFork.RepoOwner() == "" {
+				return fmt.Errorf("could not parse owner or repo name from %s", repoArg)
+			}
+		}
+	}
+
+	out := colorableOut(cmd)
+	fmt.Fprintf(out, "Forking %s...\n", utils.Cyan(ghrepo.FullName(toFork)))
+
+	return nil
 }
 
 func repoView(cmd *cobra.Command, args []string) error {
@@ -45,7 +99,7 @@ func repoView(cmd *cobra.Command, args []string) error {
 		openURL = fmt.Sprintf("https://github.com/%s", ghrepo.FullName(baseRepo))
 	} else {
 		repoArg := args[0]
-		if strings.HasPrefix(repoArg, "http:/") || strings.HasPrefix(repoArg, "https:/") {
+		if isURL(repoArg) {
 			openURL = repoArg
 		} else {
 			openURL = fmt.Sprintf("https://github.com/%s", repoArg)
